f048e920f889c19a3aa3def281b33997dcadb29a
EwertonLima2002 <ewerton.lima860@sptech.school>
2023-12-27 16:40:31 -0300

    iniciando algoritimo de gerar criptografia

---
+++ Algoritimo de CriptoGrafia/js/script.js
@@ -0,0 +1,14 @@
+function criptografar(){
+    var texto = document.getElementById("input_valor").value;
+    var chave = document.getElementById("chave").value;
+    var divCritografia = document.getElementById("div_criptografia");
+    var resultado = document.getElementById("resultado");
+ 
+}
+
+function descriptografar(){
+    var texto = document.getElementById("texto").value;
+    var chave = document.getElementById("chave").value;
+    var resultado = document.getElementById("resultado");
+    var divCritografia = document.getElementById("div_criptografia");
+}
